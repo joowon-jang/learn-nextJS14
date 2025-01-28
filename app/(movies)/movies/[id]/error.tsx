f69eec4264943132d91955e7ce1c19baec51c7c7
JoowonJang <wndnjs5248@hanmail.net>
2025-01-28 18:31:55 +0900

    Error handling
    - error.tsx에서 error 발생 시 보여줄 화면을 만들 수 있다!

---
+++ app/(movies)/movies/[id]/error.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+function Error() {
+  return <h1>Error......</h1>;
+}
+
+export default Error;
--- components/movie-videos.tsx
+++ components/movie-videos.tsx
@@ -2,9 +2,10 @@ import { API_URL } from '../app/(home)/page';
 
 async function getVideos(id: string) {
   // console.log(new Date());
-  // await new Promise((resolve) => setTimeout(resolve, 3000));
-  const response = await fetch(`${API_URL}/${id}/videos`);
-  return await response.json();
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  throw new Error('Error');
+  // const response = await fetch(`${API_URL}/${id}/videos`);
+  // return await response.json();
 }
 
 async function MovieVideos({ id }: { id: string }) {
