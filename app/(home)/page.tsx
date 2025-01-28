cddb3143f8149d33bc0fb295faf4b221a3cf3999
JoowonJang <wndnjs5248@hanmail.net>
2025-01-28 15:51:44 +0900

    Server Side
    - 브라우저에서 데이터를 요청하지 않음 (Network탭을 열어보면 API 호출이 없는 것을 볼 수 있음)
    - fetch를 수행하는 건 nodeJS(백엔드)
    - 새 탭에서 localhost:3000으로 이동해보면, 탭에 로딩 상태가 표시됨 (fetch를 백엔드에서 하고 있기 때문에 서버 로딩이 완료될 때까지 React 페이지가 나타나지 않음)

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,13 +1,17 @@
-import { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Home",
+export const metadata = {
+  title: 'Home',
 };
 
-export default function Potato() {
-  return (
-    <div>
-      <h1>Hello!</h1>
-    </div>
-  );
+const URL = 'https://nomad-movies.nomadcoders.workers.dev/movies';
+
+async function getMovies() {
+  await new Promise((resolve) => setTimeout(resolve, 3000));
+  const response = await fetch(URL);
+  const movies = await response.json();
+  return movies;
+}
+
+export default async function HomePage() {
+  const movies = await getMovies();
+  return <div>{JSON.stringify(movies, null, 2)}</div>;
 }
